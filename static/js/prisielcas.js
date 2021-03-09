5bf69bbc9ee3f911b97e0583db0ff94407834abf
marosstudenic <studenic.maros@gmail.com>
2021-03-10 00:17:36 +0100

    fix counter

--- static/js/prisielcas.js
+++ static/js/prisielcas.js
@@ -1,9 +1,7 @@
-
-var countDownDate = new Date("Mar 10, 2021 12:00:00").getTime();
+var countDownDate = new Date("Mar 10, 2021 12:01:00").getTime();
 // var countDownDate = new Date("Jan 22, 2021 12:54:00").getTime();
 var now = new Date().getTime();
 var distance = countDownDate - now;
-if (distance >0){
-    window.location.replace("../../countdown.html");
+if (distance > 0) {
+  window.location.replace("../../countdown.html");
 }
-
